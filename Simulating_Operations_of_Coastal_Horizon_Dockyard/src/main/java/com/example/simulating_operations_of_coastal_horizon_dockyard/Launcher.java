32d96d0c3feb45cbb0adc2a556100256d4fbf431
Saif Shihab <shihab.dragonsphere@gmail.com>
2026-04-05 21:39:41 +0600

    Project deleted

--- Simulating_Operations_of_Coastal_Horizon_Dockyard/src/main/java/com/example/simulating_operations_of_coastal_horizon_dockyard/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.simulating_operations_of_coastal_horizon_dockyard;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- Simulating_Operations_of_Coastal_Horizon_Dockyard/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.simulating_operations_of_coastal_horizon_dockyard {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.simulating_operations_of_coastal_horizon_dockyard to javafx.fxml;
-    exports com.example.simulating_operations_of_coastal_horizon_dockyard;
-}
