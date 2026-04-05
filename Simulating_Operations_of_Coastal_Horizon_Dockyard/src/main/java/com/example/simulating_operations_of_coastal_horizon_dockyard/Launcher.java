96995360d49372f8b2fb5e25cedd46b5df4ec442
Saif Shihab <shihab.dragonsphere@gmail.com>
2026-04-05 20:51:08 +0600

    Main project created

---
+++ Simulating_Operations_of_Coastal_Horizon_Dockyard/src/main/java/com/example/simulating_operations_of_coastal_horizon_dockyard/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.simulating_operations_of_coastal_horizon_dockyard;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
