02b4d3b82bd3ecd5b47f78a9cbc58bd69b7cb084
RazinBinRobbany <razinmahmud300@gmail.com>
2026-04-09 22:52:06 +0600

    ..

---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Razin_2420842/SafetyInspectoinController.java
@@ -0,0 +1,22 @@
+package common.coastal_horizon_dockyard.Razin_2420842;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SafetyInspectoinController
+{
+    @javafx.fxml.FXML
+    private TextField InspectionDateTextFiield;
+    @javafx.fxml.FXML
+    private ComboBox<String> checklistItems;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+        checklistItems.getItems().addAll("Fire","PPE",)
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitButton(ActionEvent actionEvent) {
+    }
+}
