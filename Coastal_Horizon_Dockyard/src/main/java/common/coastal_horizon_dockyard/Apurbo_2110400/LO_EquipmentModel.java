c5ee521eea3b4d8a98e1f59990db7885d165cd12
Apurbo Rahman <apurboavenger@gmail.com>
2026-04-11 02:44:21 +0600

    I think I want a second opinion.

---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/LO_EquipmentModel.java
@@ -0,0 +1,72 @@
+package common.coastal_horizon_dockyard.Apurbo_2110400;
+
+import java.time.LocalDate;
+
+public class LO_EquipmentModel {
+    String equipmentName, quantity, conditionStatus, locations;
+    LocalDate acquisitionDate;
+
+    public LO_EquipmentModel() {
+    }
+
+    public LO_EquipmentModel(String equipmentName, String quantity, String conditionStatus, String locations, LocalDate acquisitionDate) {
+        this.equipmentName = equipmentName;
+        this.quantity = quantity;
+        this.conditionStatus = conditionStatus;
+        this.locations = locations;
+        this.acquisitionDate = acquisitionDate;
+    }
+
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getConditionStatus() {
+        return conditionStatus;
+    }
+
+    public void setConditionStatus(String conditionStatus) {
+        this.conditionStatus = conditionStatus;
+    }
+
+    public String getLocations() {
+        return locations;
+    }
+
+    public void setLocations(String locations) {
+        this.locations = locations;
+    }
+
+    public LocalDate getAcquisitionDate() {
+        return acquisitionDate;
+    }
+
+    public void setAcquisitionDate(LocalDate acquisitionDate) {
+        this.acquisitionDate = acquisitionDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LO_EquipmentModel{" +
+                "equipmentName='" + equipmentName + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", conditionStatus='" + conditionStatus + '\'' +
+                ", locations='" + locations + '\'' +
+                ", acquisitionDate=" + acquisitionDate +
+                '}';
+    }
+}
---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/LO_EquipmentViewController.java
@@ -0,0 +1,81 @@
+package common.coastal_horizon_dockyard.Apurbo_2110400;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LO_EquipmentViewController
+{
+    @javafx.fxml.FXML
+    private TextField equipmentNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> equipmentConditionStatusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<LO_EquipmentModel, String> equipmentAvailableLocationsTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<LO_EquipmentModel, String> equipmentQuantityInStockTableColumn;
+    @javafx.fxml.FXML
+    private TextField equipmentQuantityInStockTextField;
+    @javafx.fxml.FXML
+    private TableColumn<LO_EquipmentModel, String> equipmentAcquisitionDateTableColumn;
+    @javafx.fxml.FXML
+    private TableView<LO_EquipmentModel> equipmentTableView;
+    @javafx.fxml.FXML
+    private DatePicker equipmentAcquisitionDateDP;
+    @javafx.fxml.FXML
+    private ComboBox<String> equipmentAvailableLocationsComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<LO_EquipmentModel, String> equipmentNameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<LO_EquipmentModel, String> equipmentConditionStatusTableColumn;
+    @javafx.fxml.FXML
+    private AnchorPane equipmentMainPane;
+
+    public static List<LO_EquipmentModel> dataList = new ArrayList<>();
+
+
+    @javafx.fxml.FXML
+    public void initialize() {
+
+        equipmentConditionStatusComboBox.getItems().addAll("Top-Notch", "Moderately Good", "Poor");
+        equipmentAvailableLocationsComboBox.getItems().addAll("Singapore", "Shanghai", "Dubai", "Rotterdam", "Hamburg", "Houston");
+
+        equipmentNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("equipmentName"));
+        equipmentQuantityInStockTableColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        equipmentConditionStatusTableColumn.setCellValueFactory(new PropertyValueFactory<>("conditionStatus"));
+        equipmentAvailableLocationsTableColumn.setCellValueFactory(new PropertyValueFactory<>("locations"));
+        equipmentAcquisitionDateTableColumn.setCellValueFactory(new PropertyValueFactory<>("acquisitionDate"));
+
+        equipmentTableView.getItems().setAll(dataList);
+
+    }
+
+
+
+    @javafx.fxml.FXML
+    public void equipmentUpdateLogButtonOnAction(ActionEvent actionEvent) {
+
+        String equipmentName = equipmentNameTextField.getText();
+        String quantity = equipmentQuantityInStockTextField.getText();
+        String conditionStatus = equipmentConditionStatusComboBox.getValue();
+        String locations = equipmentAvailableLocationsComboBox.getValue();
+        LocalDate acquisitionDate = equipmentAcquisitionDateDP.getValue();
+
+    }
+
+    @javafx.fxml.FXML
+    public void backOnClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LogisticsOfficerView.fxml"));
+        Node node = fxmlLoader.load();
+        equipmentMainPane.getChildren().setAll(node);
+
+    }
+}
