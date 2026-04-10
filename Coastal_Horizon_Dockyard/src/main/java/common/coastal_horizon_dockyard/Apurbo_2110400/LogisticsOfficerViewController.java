0e8542023665677394dfcbf46a53951097617912
Apurbo Rahman <apurboavenger@gmail.com>
2026-04-11 04:43:40 +0600

    I think I want a second opinion.

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/LogisticsOfficerViewController.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/LogisticsOfficerViewController.java
@@ -1,7 +1,13 @@
 package common.coastal_horizon_dockyard.Apurbo_2110400;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogisticsOfficerViewController
 {
@@ -13,34 +19,111 @@ public class LogisticsOfficerViewController
     }
 
     @javafx.fxml.FXML
-    public void logisticsWorkforceAllocationButtonOnAction(ActionEvent actionEvent) {
+    public void logisticsWorkforceAllocationButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_WorkforceView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 
-    @javafx.fxml.FXML
-    public void logisticsTrackShipmentButtonOnAction(ActionEvent actionEvent) {
-    }
+
+
 
     @javafx.fxml.FXML
-    public void logisticsDockSchedulingButtonOnAction(ActionEvent actionEvent) {
+    public void logisticsTrackShipmentButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_ShipmentView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 
-    @javafx.fxml.FXML
-    public void logisticsOperationsButtonOnAction(ActionEvent actionEvent) {
-    }
+
+
 
     @javafx.fxml.FXML
-    public void logisticsLogisticsReportButtonOnAction(ActionEvent actionEvent) {
+    public void logisticsDockSchedulingButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_ScheduleView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 
-    @javafx.fxml.FXML
-    public void logisticsCreatePurchaseOrderButtonOnAction(ActionEvent actionEvent) {
-    }
+
+
 
     @javafx.fxml.FXML
-    public void logisticsEquipmentManagementButtonOnAction(ActionEvent actionEvent) {
+    public void logisticsOperationsButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_OperationsView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 
+
+
+
     @javafx.fxml.FXML
-    public void logisticsInventoryManagementButtonOnAction(ActionEvent actionEvent) {
+    public void logisticsLogisticsReportButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_ReportView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+
+    @javafx.fxml.FXML
+    public void logisticsCreatePurchaseOrderButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_PurchaseView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+
+    @javafx.fxml.FXML
+    public void logisticsEquipmentManagementButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_EquipmentView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+
+    @javafx.fxml.FXML
+    public void logisticsInventoryManagementButtonOnAction(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/LO_InventoryView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
