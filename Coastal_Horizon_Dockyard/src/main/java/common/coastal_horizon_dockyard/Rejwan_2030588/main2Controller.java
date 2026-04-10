dd92f8bce1a7a6e1122f9ed65aba19de59bd0944
RazinBinRobbany <razinmahmud300@gmail.com>
2026-04-10 17:01:36 +0600

    fxml created

---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/main2Controller.java
@@ -0,0 +1,12 @@
+package common.coastal_horizon_dockyard.Rejwan_2030588;
+
+import javafx.scene.control.TextField;
+
+public class main2Controller
+{
+    @javafx.fxml.FXML
+    private TextField TextIffdjsm;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/mainController.java
@@ -0,0 +1,12 @@
+package common.coastal_horizon_dockyard.Rejwan_2030588;
+
+import javafx.scene.control.TextField;
+
+public class mainController
+{
+    @javafx.fxml.FXML
+    private TextField NametextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
