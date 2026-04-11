733cf421a54ea924d0aa2cb004ef6fcc2ed78738
Gvulture <rejwan150015@gmail.com>
2026-04-12 01:37:17 +0600

    7 min

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Issue_Materials.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Issue_Materials.java
@@ -1,7 +1,13 @@
 package common.coastal_horizon_dockyard.Rejwan_2030588;
 
-public class Issue_Materials
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Issue_Materials {
+
+    @FXML
+    void JumpToInventoryAndStoreKeeperOnClick(ActionEvent event) {
+
+    }
+
+}
