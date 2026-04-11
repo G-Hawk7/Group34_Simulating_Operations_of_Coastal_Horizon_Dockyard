720f10ed3c5c8d3fed00c2b33235d46bf4cf0d44
Gvulture <rejwan150015@gmail.com>
2026-04-11 14:42:10 +0600

    update

---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Verify_Welding_Quality.java
@@ -0,0 +1,12 @@
+package common.coastal_horizon_dockyard.Rejwan_2030588;
+
+import javafx.scene.control.ToggleGroup;
+
+public class Verify_Welding_Quality
+{
+    @javafx.fxml.FXML
+    private ToggleGroup pass;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
