ab70c4ef140053132bccc30137cc3ea07388c991
Saif Shihab <shihab.dragonsphere@gmail.com>
2026-04-11 19:48:52 +0600

    Testing

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/HelloApplication.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Saif_2120462/MD_ClientReports.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login");
+        stage.setTitle("Client Reports");
         stage.setScene(scene);
         stage.show();
     }
--- Coastal_Horizon_Dockyard/src/main/java/module-info.java
+++ Coastal_Horizon_Dockyard/src/main/java/module-info.java
@@ -6,6 +6,13 @@ module common.coastal_horizon_dockyard {
 
     opens common.coastal_horizon_dockyard to javafx.fxml;
     exports common.coastal_horizon_dockyard;
-    exports common.coastal_horizon_dockyard.Apurbo_2110400;
+
+    opens common.coastal_horizon_dockyard.Saif_2120462 to javafx.fxml;
+    exports common.coastal_horizon_dockyard.Saif_2120462;
     opens common.coastal_horizon_dockyard.Apurbo_2110400 to javafx.fxml;
+    exports common.coastal_horizon_dockyard.Apurbo_2110400;
+    opens common.coastal_horizon_dockyard.Rejwan_2030588 to javafx.fxml;
+    exports common.coastal_horizon_dockyard.Rejwan_2030588;
+    opens common.coastal_horizon_dockyard.Razin_2420842 to javafx.fxml;
+    exports common.coastal_horizon_dockyard.Razin_2420842;
 }
