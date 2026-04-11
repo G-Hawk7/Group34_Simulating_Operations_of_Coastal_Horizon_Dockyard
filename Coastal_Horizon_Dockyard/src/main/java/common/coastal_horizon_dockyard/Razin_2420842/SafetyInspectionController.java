e0232db06468fcfb14c04587b3f3ea52e4ded732
RazinBinRobbany <razinmahmud300@gmail.com>
2026-04-11 13:38:37 +0600

    add

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Razin_2420842/SafetyInspectionController.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Razin_2420842/SafetyInspectionController.java
@@ -1,10 +1,9 @@
 package common.coastal_horizon_dockyard.Razin_2420842;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
 
 public class SafetyInspectionController
 {
@@ -20,24 +19,40 @@ public class SafetyInspectionController
     private ComboBox<String> safetyChecklist;
     @javafx.fxml.FXML
     private Label outputLabel;
+    @javafx.fxml.FXML
+    private TableColumn issuecol;
+    @javafx.fxml.FXML
+    private TableColumn issue3col;
+    @javafx.fxml.FXML
+    private TableColumn issue2col;
+    @javafx.fxml.FXML
+    private TableColumn datecol;
+    @javafx.fxml.FXML
+    private TableColumn checklistcol;
+    @javafx.fxml.FXML
+    private TableView inspectiontableview;
 
     @javafx.fxml.FXML
     public void initialize() {
         safetyChecklist.getItems().addAll("PPE Check","Fire Safety","Equipment Safety");
     }
+    ArrayList<safetyInspection> listofsefty  = new ArrayList<>();
 
     @javafx.fxml.FXML
     public void submitActionButton(ActionEvent actionEvent) {
         String date = dateTextField.getText();
         String issue1 = issue1TextField.getText();
-        String issue2 = issue1TextField.getText();
-        String issue3 = issue1TextField.getText();
-        common.coastal_horizon_dockyard.Razin_2420842.safetyInspection newSafetyInspection = new safetyInspection(date ,issue1,issue2,issue3,checklist);
+        String issue2 = issue2TextField.getText();
+        String issue3 = issueTextField3.getText();
+        String selectedChecklist = safetyChecklist.getValue();
         if (safetyChecklist.getSelectionModel().getSelectedItem() == null){
             outputLabel.setText("please select Checklist option. ");
             return;
         }
-        String sefty = safetyChecklist.getValue();
+        common.coastal_horizon_dockyard.Razin_2420842.safetyInspection safety = new safetyInspection(date, issue1, issue2, issue3, selectedChecklist);
+        listofsefty.add(safety);
+
+
 
 
     }
