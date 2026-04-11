b7e7934aab06cf2f6b47c265e1d98978a0fc4d99
Gvulture <rejwan150015@gmail.com>
2026-04-11 15:38:40 +0600

    7 min

---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Ensure_Compliance_Standards.java
@@ -0,0 +1,7 @@
+package common.coastal_horizon_dockyard.Rejwan_2030588;
+
+public class Ensure_Compliance_Standards
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Issue_Materials.java
@@ -0,0 +1,7 @@
+package common.coastal_horizon_dockyard.Rejwan_2030588;
+
+public class Issue_Materials
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
