4370b894af45ed7257ddbdbf829c1b53348045f2
Apurbo Rahman <apurboavenger@gmail.com>
2026-04-12 03:09:42 +0600

    I think I want a second opinion.

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/MarineEngineerViewController.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Apurbo_2110400/MarineEngineerViewController.java
@@ -1,5 +1,6 @@
 package common.coastal_horizon_dockyard.Apurbo_2110400;
 
+import common.coastal_horizon_dockyard.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -21,11 +22,7 @@ public class MarineEngineerViewController
     @javafx.fxml.FXML
     public void marineCheckSafetyComplianceButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_SafetyView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_SafetyView.fxml");
 
     }
 
@@ -33,77 +30,49 @@ public class MarineEngineerViewController
     @javafx.fxml.FXML
     public void marineFuelEfficiencyAnalysisButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_FuelView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_FuelView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marineRepairMonitoringButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_RepairView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_RepairView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marineTestSystemsButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_SystemsView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_SystemsView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marineEngineeringReportButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_ReportView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_ReportView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marineEngineDiagnosisButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_DiagnosisView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_DiagnosisView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marinePropulsionInspectionButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_PropulsionView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_PropulsionView.fxml");
 
     }
 
     @javafx.fxml.FXML
     public void marineMaintenanceButtonOnAction(ActionEvent actionEvent) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Apurbo_2110400/ME_MaintenanceView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        SceneSwitcher.switchTo(actionEvent, "Apurbo_2110400/ME_MaintenanceView.fxml");
 
     }
 }
