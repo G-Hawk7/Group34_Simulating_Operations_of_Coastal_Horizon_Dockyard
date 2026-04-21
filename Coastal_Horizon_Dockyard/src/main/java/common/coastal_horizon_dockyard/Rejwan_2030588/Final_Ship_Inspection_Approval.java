ededd24766d3cc10a7666643e9e50891ceb72e3d
Gvulture <rejwan150015@gmail.com>
2026-04-22 02:51:23 +0600

    7 min

--- Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Final_Ship_Inspection_Approval.java
+++ Coastal_Horizon_Dockyard/src/main/java/common/coastal_horizon_dockyard/Rejwan_2030588/Final_Ship_Inspection_Approval.java
@@ -4,7 +4,7 @@ import common.coastal_horizon_dockyard.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class inal_Ship_Inspection_Approval {
+public class Final_Ship_Inspection_Approval {
 
     @FXML    void JumpsToQualityControlInspectorOnClick(ActionEvent event) {
         SceneSwitcher.switchTo(event, "Rejwan_2030588/Quality Control Inspector.fxml");
